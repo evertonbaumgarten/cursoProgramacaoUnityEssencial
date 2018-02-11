3e8453aa2b393f5f480b1c5375156919d4577708
Everton Vieira <everton.vieira@izyplay.com.br>
2018-02-11 16:44:47 -0200

    Movendo a nave com o Translate

--- Projeto1/Assets/Scripts/ShipBehaviour.cs
+++ Projeto1/Assets/Scripts/ShipBehaviour.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class ShipBehaviour : MonoBehaviour {
 
+
 	// Use this for initialization
 	void Start () {
 		
@@ -12,9 +13,9 @@ public class ShipBehaviour : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-        if (Input.GetKey(KeyCode.LeftArrow))
-            Debug.Log("Move para Esquerda");
-        else if (Input.GetKey(KeyCode.RightArrow))
-            Debug.Log("Move para Direita");
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+            transform.Translate(Vector2.left);
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
+            transform.Translate(Vector2.right);
     }
 }
