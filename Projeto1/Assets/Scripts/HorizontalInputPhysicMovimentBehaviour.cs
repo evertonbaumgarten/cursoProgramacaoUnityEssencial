4ef3926477331ed58ee8fdf4112e1e970abc236a
Everton Vieira <everton.vieira@izyplay.com.br>
2018-03-12 20:42:14 -0300

    Limite para o Paddle

--- Projeto1/Assets/Scripts/HorizontalInputPhysicMovimentBehaviour.cs
+++ Projeto1/Assets/Scripts/HorizontalInputPhysicMovimentBehaviour.cs
@@ -5,9 +5,18 @@ using UnityEngine;
 public class HorizontalInputPhysicMovimentBehaviour : MonoBehaviour {
 
 	public float speed = 0.5f;
-	
+	public float screenLimit;
+
+	float width;
+
+	void Start()
+	{
+		width =  GetComponent<SpriteRenderer> ().size.x;
+	}
+
 	// Update is called once per frame
 	void Update () {
+		//Aqui ocorre uma atribuição por valor
 		Vector2 currentPosition = transform.position;
 
 		if (Input.GetKey(KeyCode.RightArrow))
@@ -15,6 +24,7 @@ public class HorizontalInputPhysicMovimentBehaviour : MonoBehaviour {
 		else if (Input.GetKey(KeyCode.LeftArrow))
 			currentPosition.x -= speed;
 
-		GetComponent<Rigidbody2D> ().MovePosition (currentPosition);
+		if(currentPosition.x < screenLimit-width/2 && currentPosition.x > -(screenLimit-width/2))
+			GetComponent<Rigidbody2D> ().MovePosition (currentPosition);
 	}
 }
