037b8dca87fcdb193d5c7e7d1438174c3aa52580
Everton Vieira <everton.vieira@izyplay.com.br>
2018-03-19 20:44:09 -0300

    - Nova cena de menu com botões e paineis para seleção da dificuldade

---
+++ Projeto1/Assets/Scripts/ExerciciosAula5/MenuBehaviour.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MenuBehaviour : MonoBehaviour {
+    const string LEVEL_IZY = "IZY";
+    const string LEVEL_NORMAL = "NORMAL";
+    const string LEVEL_HARD = "HARD";
+
+    public Button StartButton;
+    public Button LevelButton;
+
+    public GameObject LevelPanel;
+    public GameObject LevelIzy;
+    public GameObject LevelNormal;
+    public GameObject LevelHard;
+
+
+    string currentLevel = LEVEL_IZY;
+
+    private void Start()
+    {
+        LevelButton.GetComponentInChildren<Text>().text = currentLevel;
+        LevelPanel.SetActive(false);
+    }
+
+    public void onStartButtonClick()
+    {
+        SceneManager.LoadScene("Scenes/ExerciciosAula5/Level"+ currentLevel, LoadSceneMode.Single);
+    }
+
+    public void onLevelButtonClick()
+    {
+        LevelPanel.SetActive(true);
+    }
+
+    public void onLevelChooseButtonClick(string option)
+    {
+        LevelPanel.SetActive(false);
+        currentLevel = option;
+        LevelButton.GetComponentInChildren<Text>().text = currentLevel;
+    }
+}
