66b8955edc4c1ee54283f0ca1d93703b26dec0d3
Everton Vieira <everton.vieira@izyplay.com.br>
2018-03-25 21:48:07 -0300

    - alteração no BehaviourMoviment para respeitar limites laterais.

--- Projeto2/Assets/Scripts/HorizontalInputMovimentBehaviour.cs
+++ Projeto2/Assets/Scripts/HorizontalInputMovimentBehaviour.cs
@@ -5,14 +5,21 @@ using UnityEngine;
 public class HorizontalInputMovimentBehaviour : MonoBehaviour {
 
     public float speed = 0.1f;
+    public float maxX, minX;
 
-	// Update is called once per frame
-	void Update () {
+    private float width;
+
+    private void Start()
+    {
+        width = GetComponent<SpriteRenderer>().size.x;    
+    }
+    // Update is called once per frame
+    void Update () {
         Vector2 currentPosition = transform.position;
 
-        if (Input.GetKey(KeyCode.RightArrow))
+        if (Input.GetKey(KeyCode.RightArrow) && currentPosition.x+width < maxX)
             currentPosition.x += speed;
-        else if (Input.GetKey(KeyCode.LeftArrow))
+        else if (Input.GetKey(KeyCode.LeftArrow) && currentPosition.x - width > minX)
             currentPosition.x -= speed;
 
         transform.position = currentPosition;
