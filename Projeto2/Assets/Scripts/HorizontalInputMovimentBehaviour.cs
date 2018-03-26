3ef0355efbe64f8ce71e5870248ffa97eb31c9f6
Everton Vieira <everton.vieira@izyplay.com.br>
2018-03-25 21:41:32 -0300

    - criação do projeto 2 Galaga - comportamento inicial da nave

---
+++ Projeto2/Assets/Scripts/HorizontalInputMovimentBehaviour.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HorizontalInputMovimentBehaviour : MonoBehaviour {
+
+    public float speed = 0.1f;
+
+	// Update is called once per frame
+	void Update () {
+        Vector2 currentPosition = transform.position;
+
+        if (Input.GetKey(KeyCode.RightArrow))
+            currentPosition.x += speed;
+        else if (Input.GetKey(KeyCode.LeftArrow))
+            currentPosition.x -= speed;
+
+        transform.position = currentPosition;
+    }
+}
---
+++ Projeto2/Assets/Scripts/ShootingProjetile2DBehaviour.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootingProjetile2DBehaviour : MonoBehaviour {
+
+    public GameObject projectilePrefab;
+    public Vector2 direction = Vector2.up;
+    public Transform positionAgent;
+    public float speed;
+
+    // Update is called once per frame
+    void Update () {
+	    if(Input.GetKeyDown(KeyCode.Space))
+        {
+            GameObject projectileTemp = Instantiate<GameObject>(projectilePrefab);
+            projectileTemp.transform.position = positionAgent.position;
+            if (projectileTemp.GetComponent<Rigidbody2D>())
+                projectileTemp.GetComponent<Rigidbody2D>().velocity = direction * speed;
+            else
+                throw new MissingComponentException("Falta Rigidibody");
+        }
+            
+	}
+}
