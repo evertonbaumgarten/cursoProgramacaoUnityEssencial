183ba3dab2d6305c3b3a9b0572dca825f894b6ce
Everton Vieira <everton.vieira@izyplay.com.br>
2018-04-09 21:09:08 -0300

    - Ajustes no Projeto 3 - Novo projeto 4

---
+++ Projeto3/Assets/Script/MoveByPhysicsAndAxisBehaviour.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveByPhysicsAndAxisBehaviour : MonoBehaviour {
+
+    public float speed;
+    public float jumpDistance;
+
+    protected bool jumping=false;
+    protected float xDirection;
+    protected Rigidbody2D rb2d;
+
+    private void OnEnable()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate () {
+
+        xDirection = Input.GetAxis("Horizontal");
+
+        if(xDirection != 0)
+            rb2d.velocity = new Vector2 (xDirection * speed, rb2d.velocity.y) ;
+
+        if (Input.GetAxis("Jump") == 1 && !jumping)
+        {
+            Debug.Log("Jump!");
+            rb2d.AddForce(Vector2.up * jumpDistance);
+        }
+
+
+        //Debug.Log(GetComponent<Rigidbody2D>().velocity);
+
+	}
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log(collision.collider.tag);
+        if(collision.collider.tag=="Ground")
+            jumping = false;
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Ground")
+            jumping = true;
+    }
+}
