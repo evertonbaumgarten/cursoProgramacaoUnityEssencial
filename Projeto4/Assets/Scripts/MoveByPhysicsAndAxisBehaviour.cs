0d0897c53394a8e71348836037e92a3aac84ac5f
Everton Vieira <everton.vieira@izyplay.com.br>
2018-04-11 22:09:22 -0300

    - projeto 3 com o piso ajustado - projeto 4 com toda a movimentação funcionando

---
+++ Projeto4/Assets/Scripts/MoveByPhysicsAndAxisBehaviour.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveByPhysicsAndAxisBehaviour : MonoBehaviour {
+
+    public static string EVENT_GO_DOWN = "goDown";
+    public static string EVENT_GO_UP = "goUp";
+    public static string EVENT_GO_RIGHT = "goRight";
+    public static string EVENT_GO_LEFT = "goLeft";
+    public static string EVENT_STOP = "stop";
+
+    public float speed;
+    public float jumpDistance;
+    public Animator animator;
+
+    protected bool jumping=false;
+    protected Rigidbody2D rb2d;
+    protected Vector2 direction;
+    
+
+    private void OnEnable()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate () {
+
+        direction = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+
+
+        if (direction.x > 0 && !animator.GetCurrentAnimatorStateInfo(0).IsName("WalkingRight"))
+        {
+            animator.SetTrigger(EVENT_GO_RIGHT);
+            transform.localScale.Set(1, 1, 1);
+        }
+        else if (direction.x < 0 && !animator.GetCurrentAnimatorStateInfo(0).IsName("WalkingLeft"))
+        {
+            animator.SetTrigger(EVENT_GO_LEFT);
+            transform.localScale.Set(-1, 1, 1);
+        }
+        else if (direction.y > 0 && !animator.GetCurrentAnimatorStateInfo(0).IsName("WalkingUp"))
+        {
+            animator.SetTrigger(EVENT_GO_UP);
+        }
+        else if (direction.y < 0 && !animator.GetCurrentAnimatorStateInfo(0).IsName("WalkingDown"))
+        {
+            animator.SetTrigger(EVENT_GO_DOWN);
+        }else if(rb2d.velocity.magnitude ==0)
+            animator.SetTrigger(EVENT_STOP);
+
+        rb2d.velocity = direction * speed;
+	}
+
+}
