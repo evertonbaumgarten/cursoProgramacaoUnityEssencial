3854d354c280cda8c5fe3795b2907fcc841dacde
Everton Vieira <everton.vieira@izyplay.com.br>
2018-10-03 20:50:26 -0300

    Ajuste na aula 3, criando uma classe simples

---
+++ Projeto1/Assets/Scripts/ExerciciosAula4/BallBehaviourSimple.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallBehaviourSimple : MonoBehaviour {
+
+	// Use this for initialization
+	public float startSpeed;
+
+	void Start () {
+
+        //Aplica uma força de 10 vezes para diagonal direita superior.
+		GetComponent<Rigidbody2D>().velocity = (Vector2.up+Vector2.right)*startSpeed;
+        //Modifica a influência da gravidade sobre este corpo para que ele se comporte como esperado.
+        //GetComponent<Rigidbody2D>().gravityScale = 0.01f;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+		print("OnCollisionEnter2D"+ collision.collider.name);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+		print("OnTriggerEnter2D "+ other.name);
+    }
+}
